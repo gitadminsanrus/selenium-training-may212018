197a974a88da40fba958adf72bc18b425f4f80ae
Santhosh Gutta <santhoshgutta@gmail.com>
2018-06-05 05:39:54 -0400

    Added javascript demo

---
+++ JavaScriptDemo/WebContent/js/home_page_js.js
@@ -0,0 +1,33 @@
+/**
+ *  Home Page javascript validations goes in this file.
+ */
+
+function callMe() {
+	//alert("Clicked on a button" + count);
+	
+//	var x = "ABC10";
+//	alert(typeof x);
+//	
+//	x = parseInt(x, 10);
+//	alert("result = " + x);
+//	alert(typeof x);
+//	
+//	x = false;
+//	alert(typeof x);
+	
+	var y = 10;
+	alert(y);
+	alert(y == 10);//checks only value equality
+	alert(y === 10);//checks value and data type
+	
+}
+
+var count = 10;
+
+function add(x, y) {
+	
+	if (typeof x == "number") {
+		
+	}
+	
+}
